32fa9f4ce9edc6f17e1e8fa243caf7248a4d41cc
casterchizeni <echizeni@cast.ai>
2025-03-14 03:29:34 -0500

    Added node template to the castai.tf
    Added node template to activate the autoscaler in pod scheduling.

--- examples/eks/eks_cluster_optional_readonly/castai.tf
+++ examples/eks/eks_cluster_optional_readonly/castai.tf
@@ -18,6 +18,7 @@ provider "helm" {
   }
 }
 
+
 resource "castai_eks_cluster" "this" {
   account_id = data.aws_caller_identity.current.account_id
   region     = var.cluster_region
@@ -92,6 +93,82 @@ resource "castai_node_configuration_default" "this" {
   configuration_id = castai_node_configuration.this[0].id
 }
 
+resource "castai_node_template" "non_critical_nodes" {
+  cluster_id       = castai_eks_cluster.this.id
+  name             = "non-critical-nodes"
+  is_default       = false
+  is_enabled       = true
+
+  # Labels must match Deployment's nodeSelector
+  custom_labels = {
+    critical = "no"
+    env      = "staging"
+    team     = "developers"
+  }
+
+  constraints {
+    on_demand = true
+    spot      = false
+    min_cpu   = 2
+    max_cpu   = 16
+    min_memory = 4096
+    max_memory = 32768
+    azs = ["us-east-2a", "us-east-2b", "us-east-2c"]
+
+    # Allow more instance families to fit workloads
+    instance_families {
+      include = ["c5", "m5", "r5"]
+    }
+
+    
+  }
+
+}
+
+
+resource "castai_node_template" "default_by_castai" {
+  cluster_id       = castai_eks_cluster.this.id
+  name             = "default-by-castai"
+  is_default       = true
+  is_enabled       = true
+  configuration_id = castai_node_configuration.this[0].id  # Link to node configuration
+  should_taint     = false  # Set to true if you want taints on nodes
+
+  custom_labels = {
+    critical = "yes"
+    env  = "staging"
+    team = "developers"
+  }
+
+
+  constraints {
+    on_demand                                   = true
+    spot                                        = false
+    use_spot_fallbacks                          = true
+    fallback_restore_rate_seconds               = 300
+    enable_spot_diversity                       = true
+    spot_diversity_price_increase_limit_percent = 20
+    spot_interruption_predictions_enabled       = true
+    spot_interruption_predictions_type          = "aws-rebalance-recommendations"
+    compute_optimized_state                     = "disabled"
+    storage_optimized_state                     = "disabled"
+    is_gpu_only                                 = false
+    min_cpu                                     = 2
+    max_cpu                                     = 8
+    min_memory                                  = 4096
+    max_memory                                  = 32768
+    #architectures                               = ["amd64"]
+    azs                                         = ["us-east-2a", "us-east-2b"]
+    burstable_instances                         = "disabled"
+    customer_specific                           = "disabled"
+
+    instance_families {
+      include = ["c5","m5","r5"]
+    }  
+
+  }
+}
+
 resource "helm_release" "castai_cluster_controller" {
   count            = var.readonly ? 0 : 1
   name             = "cluster-controller"
@@ -124,5 +201,48 @@ resource "helm_release" "castai_cluster_controller" {
 
   lifecycle {
     ignore_changes = [version]
+  }
+}
+
+resource "castai_autoscaler" "castai_autoscaler_policy" {
+  cluster_id = castai_eks_cluster.this.id
+
+  autoscaler_settings {
+    enabled                                 = true
+    is_scoped_mode                          = false
+    node_templates_partial_matching_enabled = true
+
+    unschedulable_pods {
+      enabled = true
+    }
+
+    cluster_limits {
+      enabled = true
+
+      cpu {
+        min_cores = 1
+        max_cores = 10
+      }
+    }
+
+    node_downscaler {
+      enabled = true
+
+      empty_nodes {
+        enabled       = true
+        delay_seconds = 90
+      }
+
+      evictor {
+        enabled                                = true
+        dry_run                                = false
+        aggressive_mode                        = false
+        scoped_mode                            = false
+        cycle_interval                         = "60s"
+        node_grace_period_minutes              = 10
+        pod_eviction_failure_back_off_interval = "30s"
+        ignore_pod_disruption_budgets          = false
+      }
+    }
   }
 }
